d5bb408a29c62beeed7f762a8331492dd187d128
TomBiton <tombiton2711@gmail.com>
2024-11-19 14:07:45 +0200

    Tom Biton - update SharedDATA + TestThreadCheckArray

--- ThreadCheckArray/src/SharedData.java
+++ ThreadCheckArray/src/SharedData.java
@@ -1,45 +1,79 @@
 import java.util.ArrayList;
 
-public class SharedData 
-{
-	private ArrayList<Integer> array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
-	
-	public SharedData(ArrayList<Integer> array, int b) {
-		
-		this.array = array;
-		this.b = b;
-	}
+/**
+ * This class encapsulates shared data between threads. It stores the array of integers, the target sum, 
+ * a flag indicating whether a solution has been found, and an array of boolean values marking the subset 
+ * contributing to the solution
+ */
+public class SharedData {
+    private ArrayList<Integer> array;
+    private boolean[] winArray;
+    private boolean flag;
+    private final int b;
 
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+    /**
+     * Constructs a `SharedData` object with the given array and target sum.
+     *
+     * @param array The list of integers to be checked
+     * @param b     The target sum to achieve
+     */
+    public SharedData(ArrayList<Integer> array, int b) {
+        this.array = array;
+        this.b = b;
+    }
 
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    /**
+     * Retrieves the array of boolean values marking the solution subset.
+     *
+     * @return A boolean array where each element indicates if the corresponding index 
+     *         in the array is part of the solution
+     */
+    public boolean[] getWinArray() {
+        return winArray;
+    }
 
-	public ArrayList<Integer> getArray() 
-	{
-		return array;
-	}
+    /**
+     * Sets the boolean array marking the solution subset.
+     *
+     * @param winArray A boolean array indicating the solution subset
+     */
+    public void setWinArray(boolean[] winArray) {
+        this.winArray = winArray;
+    }
 
-	public int getB() 
-	{
-		return b;
-	}
+    /**
+     * Retrieves the array of integers to be checked.
+     *
+     * @return The list of integers
+     */
+    public ArrayList<Integer> getArray() {
+        return array;
+    }
 
-	public boolean getFlag() 
-	{
-		return flag;
-	}
+    /**
+     * Retrieves the target sum.
+     *
+     * @return The target sum to achieve
+     */
+    public int getB() {
+        return b;
+    }
 
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
+    /**
+     * Checks if a solution has been found.
+     *
+     * @return `true` if a solution exists, `false` otherwise
+     */
+    public boolean getFlag() {
+        return flag;
+    }
 
+    /**
+     * Sets the flag indicating whether a solution has been found.
+     *
+     * @param flag `true` if a solution exists, `false` otherwise
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,72 +1,93 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * This class serves as the entry point to test the functionality of the `ThreadCheckArray` class. 
+ * It creates a multi-threaded environment to check whether a subset of an array can sum up to a 
+ * given target value
+ */
 public class TestThreadCheckArray {
-	public static void main(String[] args) {
-		try (Scanner input = new Scanner(System.in)) {
-			Thread thread1, thread2;
-			System.out.println("Enter array size");
-			int num  = input.nextInt();
-			ArrayList<Integer> array = new ArrayList<>();
-			System.out.println("Enter numbers for array");
-			
-			for (int index = 0; index < num; index++) 
-				array.add(index, input.nextInt());
-			
-			System.out.println("Enter number");
-			num = input.nextInt();
-			
-			SharedData sd = new SharedData(array, num);
-			
-			thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
-			thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
-			thread1.start();
-			thread2.start();
-			try 
-			{
-				thread1.join();
-				thread2.join();
-			} 
-			catch (InterruptedException e)
-			{
-				e.printStackTrace();
-			}
-			if (!sd.getFlag())
-			{
-				System.out.println("Sorry");
-				return;
-			}
-			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().size());
-			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().size() ; index++)
-				System.out.print(index + "    ");
-			System.out.println();
-			System.out.print("A:    ");
-			for (int index : sd.getArray())
-			{
-				System.out.print(index);
-				int counter = 5;
-				while (true)
-				{
-					index = index / 10;
-					counter--;
-					if (index == 0)
-						break;
-				}
-				for (int i = 0; i < counter; i++)
-					System.out.print(" ");
-			}
 
-			System.out.println();
-			System.out.print("C:    ");
-			for (boolean index : sd.getWinArray())
-			{
-				if (index)
-					System.out.print("1    ");
-				else
-					System.out.print("0    ");	
-			}
-		}
-	}
+    /**
+     * The main method initializes the array, the target sum, and shared data, and then creates 
+     * and starts threads to perform the subset sum check. The results are displayed at the end.
+     *
+     * @param args Command-line arguments (not used in this program).
+     */
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            Thread thread1, thread2;
 
+            // Input array size
+            System.out.println("Enter array size");
+            int num = input.nextInt();
+
+            ArrayList<Integer> array = new ArrayList<>();
+            System.out.println("Enter numbers for array");
+
+            // Populate the array with user input
+            for (int index = 0; index < num; index++) 
+                array.add(index, input.nextInt());
+
+            // Input the target sum
+            System.out.println("Enter number");
+            num = input.nextInt();
+
+            // Create shared data
+            SharedData sd = new SharedData(array, num);
+
+            // Create and start threads
+            thread1 = new Thread(new ThreadCheckArray(sd), "thread1");
+            thread2 = new Thread(new ThreadCheckArray(sd), "thread2");
+            thread1.start();
+            thread2.start();
+
+            // Wait for threads to complete
+            try {
+                thread1.join();
+                thread2.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Check the result and display output
+            if (!sd.getFlag()) {
+                System.out.println("Sorry");
+                return;
+            }
+
+            System.out.println("Solution for b : " + sd.getB() + ", n = " + sd.getArray().size());
+            
+            // Print indices
+            System.out.print("I:    ");
+            for (int index = 0; index < sd.getArray().size(); index++)
+                System.out.print(index + "    ");
+            System.out.println();
+
+            // Print array values
+            System.out.print("A:    ");
+            for (int index : sd.getArray()) {
+                System.out.print(index);
+                int counter = 5;
+                while (true) {
+                    index = index / 10;
+                    counter--;
+                    if (index == 0)
+                        break;
+                }
+                for (int i = 0; i < counter; i++)
+                    System.out.print(" ");
+            }
+            System.out.println();
+
+            // Print solution flags
+            System.out.print("C:    ");
+            for (boolean index : sd.getWinArray()) {
+                if (index)
+                    System.out.print("1    ");
+                else
+                    System.out.print("0    ");
+            }
+        }
+    }
 }
